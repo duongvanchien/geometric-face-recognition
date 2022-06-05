95d6bc6da6399c6e2244d54967d06c20baa9f56a
thuptit <thunv.B18CN655@stu.ptit.edu.vn>
2022-06-05 15:57:21 +0700

    update get metadata

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from lib2to3.pytree import convert
 import cv2
 import dlib
 import os
@@ -14,30 +15,45 @@ def load_images_from_folder(folder):
     return images
 imgs = load_images_from_folder("./human/duoi 20")
 #step 2: loop each folder 'duoi 20', '20 - 45', '> 45'
-left_right_cheek = [1,3,48,15,13,54]
-for img in imgs:
+
+def img_detector(img):
     convert_img = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
     face_detector = dlib.get_frontal_face_detector()
     faces = face_detector(convert_img)
     predictor = dlib.shape_predictor("shape_predictor_81_face_landmark.dat")
     face_features = predictor(image=convert_img, box=faces[0])
-    dic = dict()
-    for n in range(0, 81):
-        #step 3: get coordinates need to crop areas
-        if left_right_cheek.__contains__(n): 
-            dic[n] = [face_features.part(n).x,face_features.part(n).y]
-    #step 4: crop area and using canny algorithm detect edge
-    print(dic)
-    # lcheek_img = convert_img[dic[3][0]:dic[48][0], dic[1][1]:dic[3][1]]
-    # cv2.imshow("cropped",lcheek_img)
-    # canny = cv2.Canny(lcheek_img, 50, 100)
-    # titles = ['image', 'canny']
-    # imgs = [img, canny]
-    # for i in range(2):
-    #     plt.subplot(1, 2, i+1), plt.imshow(imgs[i], 'gray')
-    #     plt.title(titles[i])
-    #     plt.xticks([]),plt.yticks([])
-    # plt.show()
+    return convert_img,face_features
+
+def get_value_points_face(point):
+    for key,value in points_area_face:
+        if(value.__contains__(point)):
+            return key,point
+    return None
+
+
+points_area_face = {
+    "left_cheek": [1,3,48],
+    "right_cheek": [15,13,54]
+}
+
+def handle_imgs(imgs):
+    coordinates_face = dict()
+
+    for img in imgs:
+        convert_image,face_features = img_detector(img)
+        dic = dict()
+        for point in range(0, 81):
+            #step 3: get coordinates need to crop areas
+            if get_value_points_face(point): 
+                dic["left_cheek"] = [face_features.part(n).x,face_features.part(n).y]
+        #step 4: crop area and using canny algorithm detect edge
+        print(dic)
+        lcheek_img = img[dic[3][0]:dic[48][0], dic[1][1]:dic[3][1]]
+        cv2.imshow("cropped",lcheek_img)
+        cv2.waitKey(0)
+
+
+handle_imgs(imgs=imgs)
 #step 5: determined ratia area: ratio = count(pixel_edge)/count(pixel_area)
 #step 6: save data: [area: ratio, label = ('duoi 20' or '20 -45' or '> 45')]
 
